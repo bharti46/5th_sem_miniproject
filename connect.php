08e451c0e8dcbcc58086840cfbb4484464885097
bharti46 <bhartidandona25@gmail.com>
2015-11-20 21:11:01 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php 
+
+//php function to authenticate into mysql database server 
+mysql_connect("localhost","root","");//servername, username, password
+
+
+//selection to database 
+mysql_select_db("geu"); //database name 
+
+
+
+
+?>
